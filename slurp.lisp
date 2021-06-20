1a3b1f926a72209a3eea5ca5fab83d026d23f9ff
Nick <yux50000@hotmail.com>
2021-06-19 22:50:04 -0400

    Update slurp, add miscellaneous translations of the text corpus

--- slurp.lisp
+++ slurp.lisp
@@ -1,5 +1,6 @@
 (ql:quickload 'cl-json)
 (ql:quickload 'drakma)
+(ql:quickload 'pithy-xml)
 
 ;; Text opcodes
 
@@ -25,44 +26,172 @@
     (#\o . 9) (#\p . 8) (#\q . 8) (#\r . 6)
     (#\s . 7) (#\t . 6) (#\u . 8) (#\v . 9)
     (#\w . 11) (#\x . 8) (#\y . 8) (#\z . 7)
-    (#\Space . 1)))
-(defparameter *textbox-size* 206)
+    (#\Space . 6) (#\~ . 9) (#\. . 2) (#\, . 4)
+    (#\" . 5) (#\' . 6) (#\! . 4) (#\? . 9)
+    (#\) . 6) (#\( . 6)
+    (#\0 . 7) (#\1 . 7) (#\2 . 7) (#\3 . 7) (#\4 . 7)
+    (#\5 . 7) (#\6 . 7) (#\7 . 7) (#\8 . 7) (#\9 . 7)))
+(defparameter *textbox-size* 208)
 
 (defmacro comment (&rest rest) nil)
 (comment
  (defparameter *ibm-api-endpoint* "https://api.us-south.language-translator.watson.cloud.ibm.com/instances/63165d7f-1133-4eff-94bd-30c445d34bba/v3/translate?version=2018-05-01")
+
  (defparameter *ibm-api-key*
-   (with-open-file (stream "api-key.txt")
+   (with-open-file (stream "ibm-api-key.txt")
+     (read stream)))
+
+ (defparameter *deepl-api-endpoint* "https://api.deepl.com/v2/translate")
+ (defparameter *deepl-api-key*
+   (with-open-file (stream "deepl-api-key.txt")
      (read stream)))
 
  (defun jswrap (txt)
    (cl-json:encode-json-to-string `(("text" . ,(list txt)) ("model_id" . "ja-en"))))
 
- (defun translate-fragment (frag)
-   (when (and (> (length frag) 1) (not (zerop (mismatch "『" frag))))
-       (setf frag (subseq frag 1)))
-   (cdr
-    (assoc :translation
-           (cadr
-            (assoc
-             :translations
-             (cl-json:decode-json
-              (drakma:http-request *ibm-api-endpoint* :method :post
-                                                      :content-type "application/json"
-                                                      :basic-authorization (list "apikey" *ibm-api-key*)
-                                                      :content (jswrap frag)
-                                                      :want-stream t)))))))
+ (defun translate-fragment-ibm (frag)
+   (setf frag (remove #\『 frag)) ;hack
+   (if (string= frag "")
+       ""
+       (cdr
+        (assoc :translation
+               (cadr
+                (assoc
+                 :translations
+                 (cl-json:decode-json
+                  (drakma:http-request *ibm-api-endpoint* :method :post
+                                                          :content-type "application/json"
+                                                          :basic-authorization (list "apikey" *ibm-api-key*)
+                                                          :content (jswrap frag)
+                                                          :want-stream t))))))))
+
+ (defun translate-fragment-deepl (frag)
+   (setf frag (remove #\『 frag)) ;hack
+   (if (string= frag "")
+       ""
+       (cdr
+        (assoc :text
+               (cadr
+                (assoc
+                 :translations
+                 (cl-json:decode-json
+                  (drakma:http-request *deepl-api-endpoint*
+                                       :external-format-out :utf-8
+                                       :method :post
+                                       :parameters `(("auth_key" . ,*deepl-api-key*)
+                                                     ("text" . ,frag)
+                                                     ("target_lang" . "EN"))
+                                       :want-stream t))))))))
 
  (defun translate-string (string)
-   (loop for element in string collect
-     (if (stringp element)
-         (translate-fragment element)
-         element)))
+   (loop for element in string
+         collect
+         (cond
+           ((stringp element)
+            (translate-fragment-deepl element))
+           ((and (consp element) (equal (car element) 'name))
+            (list (car element) (translate-fragment-deepl (cadr element))))
+           (t element))))
 
- (translate-fragment "そんなことして だいじょうぶかな?")
+ (defun xmlify (string)
+   (let ((xmlified
+           (loop for element in (cdr string)
+                 collect
+                 (cond
+                   ((stringp element) (remove #\『 element))
+                   ((consp element)
+                    (cond
+                      ((equal (car element) 'name) '(:name))
+                      ((equal (car element) 'color) `(:color :val ,(cadr element)))
+                      ((equal (car element) 'byte) `(:byte :val ,(cadr element)))
+                      (t (cons (write-to-string (car element)) (cdr element)))))
+                   (t element)))))
+     (pithy-xml:print-xml
+      `(:text :number ,(car string)
+              ,@xmlified))))
+
+ (defun kw2int (kw)
+   (parse-integer (string kw)))
+
+ (defun un-xmlify (string)
+   (let ((parsed (xmls:parse-to-list string)))
+     (let* ((number (parse-integer (car (cdaadr parsed))))
+            (parsed (cddr parsed))
+            (decoded
+              (loop for element in parsed
+                    collect
+                    (cond
+                      ((stringp element) element)
+                      ((consp element)
+                       (cond
+                         ((equal (car element) "Name") '(name))
+                         ((equal (car element) "Color") (list 'color (parse-integer (cadr (assoc "val" (cadr element) :test #'string=)))))
+                         ((equal (car element) "Byte") (list 'byte (parse-integer (cadr (assoc "val" (cadr element) :test #'string=)))))
+                         (t (list (read-from-string (car element))))))
+                      (t "wello")))))
+       (cons number decoded))))
+
+ (defun translate-string-xml (frag)
+   (if (string= frag "")
+       ""
+       (cdr
+        (assoc :text
+               (cadr
+                (assoc
+                 :translations
+                 (cl-json:decode-json
+                  (drakma:http-request *deepl-api-endpoint*
+                                       :external-format-out :utf-8
+                                       :method :post
+                                       :parameters `(("auth_key" . ,*deepl-api-key*)
+                                                     ("text" . ,frag)
+                                                     ("target_lang" . "EN")
+                                                     ("tag_handling" . "xml")
+                                                     ("split_sentences" . "nonewlines")
+                                                     )
+                                       :want-stream t))))))))
+
+ (defun translate-string-deepl-good (string)
+   (let* ((xmled (xmlify string))
+          (deepled (translate-string-xml xmled))
+          (un-xmled (un-xmlify deepled)))
+     un-xmled))
+
+ (defun zip-name (orig new)
+   (let* ((origname (find-if (lambda (x) (and
+                                          (consp x)
+                                          (equal (car x) 'name)))
+                             orig)))
+     (loop for element in new
+           collect
+           (cond
+             ((and (consp element) (equal (car element) 'name))
+              origname)
+             (t element)))))
+
+ (setf att2
+       (loop for otxt in origcrud
+             for ntxt in *all-text-translated*
+             collect (zip-name otxt ntxt)))
+
+ (setf att3
+       (loop for string in att2
+             collect
+             (loop for element in string
+                   collect
+                   (cond
+                     ((equal element '(playername)) '(player-name))
+                     (t element)))))
+
+ (translate-fragment-deepl "そんなことして だいじょうぶかな?")
 
  (defparameter reformd9 (mapcar (lambda (x) (hack-reformat (cons (car x) (reflow-string (cdr x))))) *all-text-translated*))
+
+
+ (defparameter reformd10 (mapcar (lambda (x) (hack-reformat (cons (car x) (reflow-string (cdr x))))) att2))
+
  (defparameter slime-patched (read-rom "slime_original.gba"))
+
  (patch-rom slime-patched *inv-translation-table* reformd9)
  (dump-rom slime-patched "translated10.gba")
 
@@ -89,40 +218,41 @@
  (defun dump-all-text-utf (fname txts)
    (progn
      (with-open-file (stream fname :direction :output
-                                   :external-format :utf-8)
+                                   :external-format :utf-8
+                                   :if-exists :supersede)
        (with-standard-io-syntax
          (loop for txt in txts do
            (prin1 txt stream)
            (terpri stream))))
      nil))
+ (defun read-all-text-utf (fname)
+   (with-open-file (stream fname :direction :input
+                                 :external-format :utf-8)
+     (loop for sexp = (read stream nil)
+           while sexp
+           collect sexp)))
 
  (defparameter *all-text-translated* nil)
- (loop for string in *all-texts* do
-   (let ((translated (translate-string string)))
+
+ (loop for string in (nthcdr 1403 *all-texts*) do
+   (let* ((stripped (remove-reflow-opcodes string))
+          (translated (translate-string-deepl-good stripped)))
      (print translated)
      (push translated *all-text-translated*)))
 
- (defun old-reflow-string (string)
-   (setf string (remove-if (lambda (x) (equal x '(byte 2))) string))
-   (let (output (i 0))
-     (loop for elm in string
-           finally (return (reverse output)) do
-       (cond
-         ((stringp elm)
-          (let ((str-build ""))
-            (loop for char across elm do
-              (setf str-build (concatenate 'string str-build (list char)))
-              (when (>= i 21)
-                (setf i 0)
-                (push str-build output)
-                (push '(byte 2) output)
-                (setf str-build ""))
-              (incf i))
-            (when (not (string= str-build ""))
-              (push str-build output))))
-         (t (when (equal elm '(byte 5))
-            (setf i 0))
-          (push elm output))))))
+ (loop for string in *all-texts* do
+   (let* ((stripped (remove-reflow-opcodes string))
+          (translated (translate-string stripped)))
+     (print translated)
+     (push translated *all-text-translated*)))
+
+ (defparameter *translated-reflowed*
+   (mapcar #'reflow-string *all-text-translated*))
+
+ (defun trans (f)
+   (let ((slime-patched (read-rom "slime_original.gba")))
+     (patch-rom slime-patched (invert-alist *translation-table*) (invert-alist *small-translation-table*) (mapcar #'reflow-string *all-text-translated*))
+     (dump-rom slime-patched f)))
 
  (setf *print-vector-length* nil)
  (setf *print-length* 50)
@@ -132,13 +262,15 @@
  (defparameter *pointer-table2* (subseq *pointer-table* 53 2299))
  (defparameter *translation-table* (reverse (load-translation-table "SlimeDialogJIS.tbl")))
  (defparameter *small-translation-table* (reverse (load-translation-table "Slime_SmallJIS.tbl")))
- (defparameter *ignore* '(1982 1895 1897 1900 50))
+ (defparameter *ignore* (append (range 52) '(1892 1895 1897 1900 50)))
  (defparameter *all-texts*
    (handler-bind ((malformed-string-error #'skip-malformed-string))
      (load-all-texts *rom-data* *pointer-table* *translation-table* *small-translation-table* *ignore*)))
 
  (defun find-ptr (x)
-   (+ (car pointer-table-pos) (* 4 x))))
+   (+ (car pointer-table-pos) (* 4 x)))
+
+ )
 
 (define-condition malformed-string-error (error)
   ((text :initarg :text :reader text)))
@@ -154,6 +286,13 @@
                         nil))
         when entry collect (cons idx entry)))
 
+(defun take-while (pred list)
+  (loop for x in list
+        while (funcall pred x)
+        collect x))
+(defun range (max &key (min 0) (step 1))
+  (loop for n from min below max by step
+        collect n))
 (defun invert-alist (alist)
   (loop for (a . b) in alist
         collect (cons b a)))
@@ -187,41 +326,52 @@
 (defun reflow-string (str)
   (flet ((word-width (word)
            (loop for char across word
-                 sum (or (cdr (assoc char *letter-sizes*)) 6))))
+                 sum (let ((width (cdr (assoc char *letter-sizes*))))
+                       (if width (1+ width) 11)))))
     (let ((out nil)
           (cur-str "")
           (pixels 0)
           (need-wait nil))
       (loop for elem in str do
-        (if (not (stringp elem))
-            (progn
-              (when (not (string= cur-str ""))
-                (push cur-str out))
-              (push elem out)
-              (setf cur-str ""))
-            (let ((words (my-split elem (lambda (x) (string= x " ")))))
-              (loop for word in words do
-                (progn
-                  (incf pixels (1+ (word-width word)))
-                  (when (>= pixels *textbox-size*)
-                    (when (not (string= cur-str ""))
-                      (push cur-str out))
-                    (when need-wait
-                      (push '(byte 7) out)
-                      (push '(byte 8) out))
-                    (setf need-wait (not need-wait))
-                    (push '(newline) out)
-                    (setf pixels 0)
-                    (setf cur-str ""))
-                  (setf cur-str
-                        (concatenate 'string
-                                     cur-str
-                                     (when (not (string= cur-str "")) " ")
-                                     word))))
-              (when (not (string= cur-str ""))
-                (push cur-str out)))))
+        (cond
+          ((stringp elem)
+           (let ((words (my-split elem (lambda (x) (string= x " ")))))
+             (loop for word in words do
+               (progn
+                 (incf pixels (word-width word))
+                                        ;(format t "~a ; word-width = ~a ; pixels = ~a~%" word (word-width word) pixels)
+                 (when (< pixels *textbox-size*)
+                   (incf pixels (cdr (assoc #\Space *letter-sizes*))))
+                 (when (>= pixels *textbox-size*)
+                                        ;(format t "Splitting before ~a, ~a >= ~a~%" word pixels *textbox-size*)
+                   (when (not (string= cur-str ""))
+                     (push cur-str out))
+                   (when need-wait
+                     (push '(byte 7) out)
+                     (push '(byte 8) out))
+                   (setf need-wait (not need-wait))
+                   (push '(newline) out)
+                   (setf pixels (word-width word))
+                   (setf cur-str ""))
+                 (setf cur-str
+                       (concatenate 'string
+                                    cur-str
+                                    (when (not (string= cur-str "")) " ")
+                                    word))))
+             (when (not (string= cur-str ""))
+               (push cur-str out))))
+          ((equal elem '(player-name))
+           (push elem out)
+           (setf cur-str "")
+           (incf pixels (word-width "WWWW"))) ; hack: assume 4 widest characters
+          (t
+           (push elem out)
+           (setf cur-str ""))))
       (when (equal (car out) '(newline))
         (setf out (cdr out)))
+      (when (not (equal (car out) '(byte 8)))
+        (push '(byte 7) out)
+        (push '(byte 8) out))
       (reverse out))))
 
 (defun decode-small-string (translation-table small-translation-table offset rom &optional (cur-str ""))
@@ -257,10 +407,12 @@
 (defun encode-string (inv-translation-table inv-translation-table-small string)
   (flet ((encode-str (string tbl)
            (loop for char across string
-                 for encoded = (assoc char tbl :test #'string=)
-                 collect (if encoded (cdr encoded) 94))))
+                 for encoded = (or (assoc char tbl :test #'string=)
+                                   (assoc "?" tbl :test #'string=))
+                 collect (cdr encoded))))
     (cond
       ((null string) nil)
+      ((null (car string)) (encode-string inv-translation-table inv-translation-table-small (cdr string))) ; hack to remove misplaced nils from translator api
       ((stringp (car string))
        (append
         (encode-str (car string) inv-translation-table)
@@ -279,7 +431,7 @@
       ((equal (caar string) 'player-name)
        (cons #x0e (encode-string inv-translation-table inv-translation-table-small (cdr string))))
       ((equal (caar string) 'byte)
-       (cons (cadar string) (encode-string inv-translation-table inv-translation-table-small (cdr string))))))))
+       (cons (cadar string) (encode-string inv-translation-table inv-translation-table-small (cdr string)))))))
 (defun verify-isomorphic (string tt tts)
   (let* ((encoded (encode-string (invert-alist tt) (invert-alist tts) string))
          (decoded (decode-string tt tts 0 (append encoded '(0 0)))))
@@ -298,7 +450,7 @@
                                          (elt table-data (+ i 2))
                                          (elt table-data (+ i 3)))))))
 
-(defun patch-rom (rom encoding-table new-strings)
+(defun patch-rom (rom encoding-table small-encoding-table new-strings)
   (flet ((insert-string (bytes)
            (loop for byte in bytes do
              (cond
@@ -311,9 +463,9 @@
                (t (vector-push byte rom))))
            (vector-push 0 rom))) ; null terminator
     (let ((pointer-patches nil))
-      (loop for (table-index string) in new-strings do
+      (loop for (table-index . string) in new-strings do
         (push (cons table-index (fill-pointer rom)) pointer-patches)
-        (insert-string (encode-string encoding-table string)))
+        (insert-string (encode-string encoding-table small-encoding-table string)))
       (loop for (table-index . pointer) in pointer-patches do
         (let* ((tbl-pos (+ (car pointer-table-pos) (* 4 table-index)))
                (table-entry (logior #x8000000 pointer)))
@@ -334,4 +486,5 @@
                           :element-type '(unsigned-byte 8)
                           :direction :output
                           :if-exists :supersede)
-    (write-sequence rom stream)))
+    (write-sequence rom stream)
+    nil))
